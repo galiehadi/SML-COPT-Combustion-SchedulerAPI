9a38bd013462ce928354f397cfb2da04fc2ec81a
ichsansan <ichsan.application@gmail.com>
2022-03-31 16:10:55 +0700

    PCT1 initialize

--- db_config.py
+++ db_config.py
@@ -2,12 +2,12 @@
 Setting parameter untuk koneksi ke database.
 '''
 
-_UNIT_CODE_ = "TAA1"
-_UNIT_NAME_ = "PLTU Tanjung Awar-Awar 1"
+_UNIT_CODE_ = "PCT1"
+_UNIT_NAME_ = "PLTU Pacitan Unit 1"
 _USER_ = 'root'
 _PASS_ = 'P@ssw0rd'
-_IP_ = '192.168.1.10:3306'
-_LOCAL_IP_ = 'localhost'
-_DB_NAME_ = 'db_bat_taa1'
+_IP_ = '0.0.0.0:3306'
+_LOCAL_IP_ = '0.0.0.0'
+_DB_NAME_ = 'db_bat_pct1'
 
 SAFEGUARD_TAG = "SAFEGUARD:COMBUSTION"
